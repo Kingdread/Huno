9fc0d33beb7c75fdd4ebf0d35e452985e539115f
Daniel <kingdread@gmx.de>
2014-12-28 00:36:16 +0100

    kind of working

--- src/Cards.hs
+++ src/Cards.hs
@@ -1,5 +1,7 @@
 module Cards where
 
+import System.Random
+
 data Color = Red | Blue | Green | Yellow deriving (Eq, Show, Read, Enum)
 data Value = One | Two | Three | Four | Five | Six | Seven | Eight |
              Nine | Zero | Take2 | Skip | Reverse deriving (Eq, Show, Read, Enum)
@@ -8,20 +10,56 @@ data Card = Card Color Value | Pick | Pick4 | Picked Color deriving (Eq, Show, R
 type Deck = [Card]
 type Hand = [Card]
 
+data Player = AiPlayer String Hand | HumanPlayer String Hand deriving (Show)
+
+data Game = Game { topCard :: Card
+                 , players :: [Player]
+                 , stack   :: Deck
+                 , ntake   :: Int
+                 , nskip   :: Bool
+                 } deriving (Show)
+
+data Move = Play Card | Draw deriving (Eq, Show)
+
+getName :: Player -> String
+getName (AiPlayer n _)    = n
+getName (HumanPlayer n _) = n
+
+getCards :: Player -> Hand
+getCards (AiPlayer _ c)    = c
+getCards (HumanPlayer _ c) = c
+
+addCards :: [Card] -> Player -> Player
+addCards a (AiPlayer n c)    = AiPlayer n (a ++ c)
+addCards a (HumanPlayer n c) = HumanPlayer n (a ++ c)
+
+applyHand :: (Hand -> Hand) -> Player -> Player
+applyHand f (AiPlayer n c)    = AiPlayer n (f c)
+applyHand f (HumanPlayer n c) = HumanPlayer n (f c)
+
 
 -- | Returns whether whether card2 can be played on top of card1
-isValidMove :: Card -> Card -> Bool
+isValidMove :: Game -> Card -> Bool
+isValidMove game card = if ntake game == 0 then isValidMove_ (topCard game) card
+                        else case card of
+                            (Card _ Take2) -> not (isPick4 . topCard $ game)
+                            _ -> False
+                        where
+                        isPick4 (Picked _) = True
+                        isPick4 _          = False
+
+isValidMove_ :: Card -> Card -> Bool
 -- Normal cards, either color matches or value matches
-isValidMove (Card c1 v1) (Card c2 v2) = c1 == c2 || v1 == v2
+isValidMove_ (Card c1 v1) (Card c2 v2) = c1 == c2 || v1 == v2
 -- You can put the black ones on top of everything
-isValidMove _ Pick  = True
-isValidMove _ Pick4 = True
+isValidMove_ _ Pick  = True
+isValidMove_ _ Pick4 = True
 -- Color picked
-isValidMove (Picked c1) (Card c2 _) = c1 == c2
+isValidMove_ (Picked c1) (Card c2 _) = c1 == c2
 
 
 -- | Takes a Hand and returns a list of every card that could be played
-validMoves :: Card -> Hand -> [Card]
+validMoves :: Game -> Hand -> [Card]
 validMoves = filter . isValidMove
 
 
@@ -33,3 +71,16 @@ deck = [(Card c Zero) | c <- [Red ..]]
     where
     dup = concatMap (replicate 2)
     quad = dup . dup
+
+-- | Returns a randomly shuffled deck. Algorithm very inefficient
+shuffled :: Deck -> IO Deck
+shuffled [] = return []
+shuffled d  = do
+    index <- getStdRandom (randomR (0, length d - 1))
+    newdeck <- shuffled . takeAway d $ index
+    return ((d !! index) : newdeck)
+    where
+    takeAway [] _         = []
+    takeAway (x:xs) index = if index == 0 then xs
+                            else x : (takeAway xs (index - 1))
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,177 @@
-module Main (main) where
+module Main where
 import Cards
+import Data.IORef
+import System.IO
+import Text.Read
+
+-- | Returns a Move that the current Player would do. Randomly picks one for
+-- the AI, asks the Player in case of a HumanPlayer
+getMove :: Game -> IO Move
+getMove g = getMove_ g (last . players $ g)
+
+getMove_ :: Game -> Player -> IO Move
+getMove_ game (AiPlayer _ hand)       = case validMoves game hand of
+                                           (x:_) -> return (Play x)
+                                           [] -> return Draw
+getMove_ game (HumanPlayer name hand) = do
+    putStrLn ("Player " ++ name ++ ":")
+    putStrLn ("Your hand is: " ++ show hand)
+    let moves = Draw : (map Play . validMoves game $ hand)
+    putStr . formatMoves $ moves
+    answer <- getInputBelow (length moves)
+    return (moves !! answer)
+    where
+    formatMoves :: [Move] -> String
+    formatMoves m = fM m 0
+    fM (x:xs) i = (show i) ++ ".: " ++ (show x) ++ "\n" ++ fM xs (i + 1)
+    fM [] _     = ""
+    getInputBelow limit = do
+        putStr "Input a number: "
+        hFlush stdout
+        inp <- getLine
+        case (readMaybe inp :: Maybe Int) of
+            Just x -> if x < limit then return x
+                      else getInputBelow limit
+            Nothing -> getInputBelow limit
+
+
+-- | Takes a list of "skeleton players" and deals the (shuffled) cards.
+makeGame :: [Player] -> IO Game
+makeGame players = do
+    d <- shuffled deck
+    let (pl, nd) = deal players [] d
+    let (top:stack) = nd
+    return Game { topCard = top
+                , players = pl
+                , stack   = stack
+                , ntake   = 0
+                , nskip   = False
+                }
+    where
+    deal [] pls cards     = (pls, cards)
+    deal (p:ps) pls cards = let (player_cards, remainder) = splitAt 7 cards
+                            in deal ps (withCards p player_cards : pls) remainder
+    withCards (AiPlayer n _)    = AiPlayer n
+    withCards (HumanPlayer n _) = HumanPlayer n
+
+
+-- | Performs a single game round, usually called in a loop
+gameRound :: Game -> IO Game
+gameRound initial = do
+    let pname = getName . head . players $ initial
+    let pcards = length . getCards . head . players $ initial
+    putStrLn ("\n\nThe top card is " ++ (show . topCard $ initial))
+    putStrLn ("It's " ++ pname ++ " turn, " ++ (show pcards) ++ " cards left")
+    ludus <- newIORef initial
+    -- Advance to the next player
+    modifyIORef' ludus (\g -> g { players = rotate . players $ g })
+    game <- readIORef ludus
+    if nskip game then do
+        putStrLn (pname ++ " skips the round")
+        return game { nskip = False }
+    else do
+        move <- getMove game
+        if move == Draw then do
+            let nt = ntake game
+            if nt == 0 then do
+                putStrLn (pname ++ " takes a single card")
+                modifyIORef ludus (draw 1)
+            else do 
+                putStrLn (pname ++ " takes " ++ (show nt) ++ " cards")
+                modifyIORef ludus (draw nt)
+                modifyIORef ludus (\g -> g { ntake = 0 })
+            game <- readIORef ludus
+            move <- getMove game
+            doMove move game
+        else
+            doMove move game
+    where
+    filterOne :: (a -> Bool) -> [a] -> [a]
+    filterOne _ []     = []
+    filterOne f (x:xs) = if f x then x : (filterOne f xs)
+                         else xs
+    draw :: Int -> Game -> Game
+    draw n g = let (taken, st) = splitAt n (stack g)
+               in g { players = updateLastPlayer (++taken) (players g)
+                    , stack = st
+                    }
+    updateLastPlayer :: (Hand -> Hand) -> [Player] -> [Player]
+    updateLastPlayer f p = let (pls, (last:_)) = splitAt ((length p) - 1) p
+                           in pls ++ [applyHand f last]
+    cname :: Game -> String
+    cname = getName . last . players
+    doMove :: Move -> Game -> IO Game
+    doMove (Play card) g = do
+        putStrLn ((cname g) ++ " decides to play " ++ show card)
+        case card of
+            Pick -> do
+                color <- getColor . last . players $ g
+                putStrLn ("The picked color is " ++ (show color))
+                return g { topCard = Picked color
+                         , players = updateLastPlayer (filterOne (/= card)) (players g)
+                         }
+            Pick4 -> do
+                color <- getColor . last . players $ g
+                putStrLn ("The picked color is " ++ (show color))
+                return g { topCard = Picked color
+                         , ntake = 4
+                         , players = updateLastPlayer (filterOne (/= card)) (players g)
+                         }
+            Card _ Reverse -> do
+                return g { topCard = card
+                         , players = reverse . updateLastPlayer (filterOne (/= card)) . players $ g
+                         }
+            Card _ Take2 -> do
+                return g { topCard = card
+                         , players = updateLastPlayer (filterOne (/= card)) (players g)
+                         , ntake = ntake g + 2
+                         }
+            Card _ Skip -> do
+                return g { topCard = card
+                         , players = updateLastPlayer (filterOne (/= card)) (players g)
+                         , nskip = True
+                         }
+            Card _ _ -> do
+                return g { topCard = card
+                         , players = updateLastPlayer (filterOne (/= card)) (players g)
+                         }
+    doMove Draw g = do
+        putStrLn ((cname g) ++ " can't play a card")
+        return g
+
+
+-- | Returns a color that the player picks
+getColor :: Player -> IO Color
+getColor a@(AiPlayer _ hand) = let getCol [] = Blue
+                                   getCol (h:hs) = case h of
+                                                       Card col _ -> col
+                                                       _ -> getCol hs
+                               in return . getCol $ hand
+getColor h@(HumanPlayer _ _) = do
+    putStr "Input a color (Red, Blue, Yellow, Green): "
+    hFlush stdout
+    inp <- getLine
+    case (readMaybe inp :: Maybe Color) of
+        Just c -> return c
+        Nothing -> getColor h
+
+-- | "Cycles" a list by putting the first element at the end
+rotate :: [a] -> [a]
+rotate []     = []
+rotate (x:xs) = xs ++ [x]
+
+
+-- | Returns True if a game is won
+won :: Game -> Bool
+won = any (null . getCards) . players
+
 -- | Run the main UNO game
-main = print deck
+main = do
+    game <- makeGame [HumanPlayer "Homo Sapiens" [], AiPlayer "Elisa" []]
+    gameLoop game
+    where
+    gameLoop :: Game -> IO ()
+    gameLoop g = if won g then do
+                    let winner = last . players $ g
+                    putStrLn ((getName winner) ++ " has won the game")
+                 else gameRound g >>= gameLoop
